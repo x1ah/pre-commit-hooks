aa343f2d96a1581f477f7549e14bce1215eb8648
x1ah <x1ahgxq@gmail.com>
2019-04-11 22:58:49 +0800

    go fmt pre-commit

--- go-fmt.sh
+++ go-fmt.sh
@@ -18,9 +18,9 @@ unformatted=$(gofmt -l $gofiles)
 
 # Some files are not gofmt'd. Print message and fail.
 
-echo >&2 "Go files must be formatted with gofmt. Please run:"
 for fn in $unformatted; do
-	echo >&2 "  gofmt -w $PWD/$fn"
+    gofmt -w $PWD/$fn
+	echo >&2 "  gofmt $fn succeed."
 done
 
 exit 1
